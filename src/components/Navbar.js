e691da5b492bfe2c185185f31e02931f2e126267
mo <mo@motanveer.com>
2019-11-16 16:47:18 -0500

    Large icons, more spacing, responsive fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
 import HomeIcon from '@material-ui/icons/Home'
 import SearchIcon from '@material-ui/icons/Search';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import PersonIcon from '@material-ui/icons/Person';
 import {Link} from "react-router-dom";
 
@@ -17,7 +17,8 @@ const styles = {
       width: '100%',
       position: 'fixed',
       bottom: 0,
-      margin: '0 auto'
+      margin: '0 auto',
+      marginBottom: '10px'
   };
 
 class Navbar extends Component {
@@ -27,11 +28,11 @@ class Navbar extends Component {
 
 
         <BottomNavigation style={styles}>
-            <BottomNavigationAction label="home" value="home" icon={<HomeIcon />} href={"/home"} />
-            <BottomNavigationAction label="search" value="search" icon={<SearchIcon />} href={"/search"} />
-            <BottomNavigationAction label="upload" value="upload" icon={<AddCircleIcon />} href={"/upload"}/>
-            <BottomNavigationAction label="notifcations" value="notifications" icon={<NotificationsIcon />} href={"/notifications"} />
-            <BottomNavigationAction label="profile" value="profile" icon={<PersonIcon />} href={"/profile"} />
+            <BottomNavigationAction label="home" value="home" icon={<HomeIcon fontSize="large"/>} href={"/home"} />
+            <BottomNavigationAction label="search" value="search" icon={<SearchIcon fontSize="large"/>} href={"/search"} />
+            <BottomNavigationAction label="upload" value="upload"  icon={<AddCircleIcon fontSize="large" color="primary" />} href={"/upload"}/>
+            <BottomNavigationAction label="notifcations" value="notifications" icon={<FavoriteIcon fontSize="large"/>} href={"/notifications"} />
+            <BottomNavigationAction label="profile" value="profile" icon={<PersonIcon fontSize="large"/>} href={"/profile"} />
         </BottomNavigation>
 
 
