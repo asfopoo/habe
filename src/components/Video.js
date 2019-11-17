4b1ce1ea2ea1ebca83909fe495853fef6c04f49b
mo <mo@motanveer.com>
2019-11-16 20:46:38 -0500

    Stuff

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,7 @@ class Video extends Component
     render()
     {
         return(
-            <ReactPlayer height={700}  muted={true} loop={true}
+            <ReactPlayer height={700}  muted={true}
             url="https://www.returnlogic.com/hubfs/R2018_ASSETS/HOME_ASSETS/iPhone%20Animation/Shopify-Returns-Management-App-Demo-Video-by-Return-Logic.webm"  
             playing />
         )
