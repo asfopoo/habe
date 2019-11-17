2f84a640b74426d740e4898460676e4bb8397b2a
Journey Needham <journey@Journeys-MacBook-Pro.local>
2019-11-17 03:02:36 -0500

    Fixed Cupcakes

--- src/components/Login.js
+++ src/components/Login.js
@@ -28,11 +28,12 @@ class Login extends Component {
 
   render(){
     return(
-      <div style={{display: 'inline-flex',  justifyContent:'center', alignItems:'center', height: '100vh', 'flex-direction': 'column',  marginLeft: '40%'}}>
+      <div style={{ textAlign: 'center'}}>
+      <div style={{display: 'inline-flex',  justifyContent:'center', alignItems:'center', height: '100vh', 'flex-direction': 'column'}}>
         <img src={require('../Håbe.svg')}  alt={'none'} style={{marginBottom: '3%'}}/>
-        <h5> An easier way to find interesting and relevant content </h5>
-        <input type="text" placeholder="Email" value={this.state.email} onChange={this.handleChangeEmail} style={{marginBottom: '4%'}}  />
-        <input type="password" placeholder="Password" value={this.state.password} onChange={this.handleChangePassword} style={{marginBottom: '7%'}}  />
+        <h5 style={{ fontSize: '16px', color: 'gray', fontFamily: 'Roboto, sans-serif'}}> An easier way to find interesting and <br></br>relevant content </h5>
+        <input type="text" placeholder="Email" value={this.state.email} onChange={this.handleChangeEmail} style={{marginBottom: '4%', padding: '5px', borderRadius: '5px', border: '1px solid #d9d9d9', fontSize: '16px'}}  />
+        <input type="password" placeholder="Password" value={this.state.password} onChange={this.handleChangePassword} style={{marginBottom: '7%', padding: '5px', borderRadius: '5px', border: '1px solid #d9d9d9', fontSize: '16px'}}  />
         {this.state.email === 'mo@motanveer.com' && this.state.password === 'applesauce' ? (
           <Button variant="contained" style={{backgroundColor: '#6646EE', color: '#FFFFFF', marginBottom: '4%'}} href={"/home"}>
             Login
@@ -44,6 +45,7 @@ class Login extends Component {
           )}
 
         <a href={'/login'}> Forgot Password </a>
+      </div>
       </div>
     )
   }
