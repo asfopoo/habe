32ab995deba9a128a7a253ea87ab5b511ee139f6
Edward Nardo <edward.nardo@bd.com>
2020-02-27 15:38:44 -0500

    reademe image

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -13,7 +13,15 @@ class SignUp extends Component {
   render(){
 
     return(
-      <div className="customCenterA">
+      <div style={{
+          'margin-top': '15%',
+          'display': 'flex',
+          'flex-direction': 'column',
+          'justify-content': 'center',
+          'align-self': 'center',
+          'align-items': 'center',
+          'justify-self': 'center',
+      }}>
         <img src={require('../Assets/Images/Håbe.svg')} alt={'none'} style={{width:'220px'}}/>
         <h5 style={{fontFamily: 'Roboto'}}> An easier way to find interesting and relevant content </h5>
         <Button variant="contained" style={{backgroundColor: '#6646EE', color: '#FFFFFF'}} href={"/signMo"}>
